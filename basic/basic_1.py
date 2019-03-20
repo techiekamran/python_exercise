8d72a6f55d04e0df1ac6b5415ed179638e0103d4
kamrankh068 <kamrankh068@gmail.com>
2019-03-20 11:25:41 +0530

    python basic examples

---
+++ basic/basic_1.py
@@ -0,0 +1,10 @@
+# Write a Python program that accepts an integer (n) and computes the value of n+nn+nnn. Go to the editor
+# Sample value of n is 5 
+# Expected Result : 615
+
+
+
+
+n = str(input("Enter the any number"))
+result = int(n) + int(n*2) + int(n*3)
+print("Your result is ",result)
---
+++ basic/basic_2.py
@@ -0,0 +1,10 @@
+
+# Create a program that asks the user to enter their name and their age.
+# Print out a message addressed to them that tells them the year that they will turn 100 years old.
+
+name = input("Enter the name ")
+age = int(input("Enter your age "))
+
+result = (100-age)+2018+-
+
+print(f"{name} your are become 100 years old in the year of",result)
